21b54982bffa56796597ff0428f12903041c132a
Angelo Teixeira <imnotteixeira@gmail.com>
2018-07-05 13:59:46 +0100

    Basic Backend Routes Done. (GET, POST and DELETE)

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 //Bodypasrser Middleware
@@ -12,13 +14,19 @@ app.use(bodyParser.json());
 //DB URI
 const db = process.env.DB_URI; 
 
+
 // Connect to DB
 mongoose
-    .connect(db)
+    .connect(db, { useNewUrlParser: true })
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
-const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => res.send('hello'));
+//Use Routes
+app.use('/api/items', items);
+
+const PORT = process.env.PORT || 5000;
 
 //Start the Server
 app.listen(PORT, () => console.log(`Server started on Port ${PORT}!`));
