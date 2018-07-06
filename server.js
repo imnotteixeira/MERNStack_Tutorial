b8cd5bb6778eac753fb3aa5b8b79616cae6614b8
Angelo Teixeira <imnotteixeira@gmail.com>
2018-07-06 11:07:30 +0100

    Fixing DB connection vars

--- server.js
+++ server.js
@@ -12,16 +12,6 @@ const app = express();
 //Bodypasrser Middleware
 app.use(bodyParser.json());
 
-//DB URI
-const db = process.env.DB_URI; 
-
-
-// Connect to DB
-mongoose
-    .connect(db, { useNewUrlParser: true })
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
-
 //Use Routes
 app.use('/api/items', items);
 
@@ -35,7 +25,19 @@ if(process.env.NODE_ENV === 'production') {
     })
 }
 
-const PORT = process.env.PORT || 5000;
 
-//Start the Server
-app.listen(PORT, () => console.log(`Server started on Port ${PORT}!`));
+const DB_OPTIONS = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
+ 
+mongoose.connect(process.env.DB_URI, DB_OPTIONS);
+var conn = mongoose.connection;             
+ 
+conn.on('error', console.error.bind(console, 'connection error:'));  
+
+const PORT = process.env.PORT || 5000;
+ 
+conn.once('open', function() {
+  // Wait for the database connection to establish, then start the app.                         
+  app.listen(PORT, () => console.log(`Server started on Port ${PORT}!`));
+});
+
